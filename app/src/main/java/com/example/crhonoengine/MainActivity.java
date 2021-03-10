2ae03e175b5ff5bd06bd8d1986f5a76253f277b8
CBarneron <71774149+CBarneron@users.noreply.github.com>
2021-03-10 14:58:08 +0100

    Android
    Test

---
+++ app/src/main/java/com/example/crhonoengine/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.crhonoengine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+              
+
+
+    }
+
+}
