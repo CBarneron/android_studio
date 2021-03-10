858ada324163524d9b64fcd0fd86f22fa9ee7b55
CBarneron <71774149+CBarneron@users.noreply.github.com>
2021-03-10 15:30:01 +0100

    Btn click
    Btn click add

--- app/src/main/java/com/example/crhonoengine/MainActivity.java
+++ app/src/main/java/com/example/crhonoengine/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-              
+
+
+        Button btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                helloTextView.setText("button ok");
+
+            }
+        });
+        Button btn_stop =(Button) findViewById(R.id.btn_stop);
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v){
+                helloTextView.setText("button ok");
+            }
+            });
 
 
     }
